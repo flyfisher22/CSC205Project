39f7430f0b56de6290eb5d5dd7b124b87a097ac8
CapitalCrime <33940781+CapitalCrime@users.noreply.github.com>
2017-11-29 22:35:05 -0800

    Add files via upload
    Made snowballs resizable by holding fire button. Added AI functions.

---
+++ Snow/Assets/Assets/CameraScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+    // Use this for initialization
+    GameObject player;
+
+	void Start () {
+        player = GameObject.Find("Player");
+        Debug.Log(player);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.rotation = player.transform.rotation;
+        transform.position = player.transform.position + transform.forward * -10 + transform.up * 2;
+	}
+}
---
+++ Snow/Assets/Assets/SnowballScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowballScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+
+    void Update()
+    {
+        move();
+    }
+
+    void move()
+    {
+        transform.position += transform.forward * 10 * Time.deltaTime;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.collider.tag);
+        if (collision.collider.tag != "Player" && collision.collider.tag != "FiringPoint")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
