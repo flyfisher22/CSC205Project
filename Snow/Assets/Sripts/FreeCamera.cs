faeeca6d05f500332368a6e14caf98619adec40b
flyfisher22 <lance.s.chaplin@gmail.com>
2017-12-02 22:05:29 -0800

    basic lvl design

---
+++ Snow/Assets/Sripts/FreeCamera.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreeCamera : MonoBehaviour {
+    float MovementSpeed = 50;
+
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += (transform.forward * Time.deltaTime * MovementSpeed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= (transform.forward * Time.deltaTime * MovementSpeed);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.position += (transform.up * Time.deltaTime * MovementSpeed);
+        }
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            transform.position -= (transform.up * Time.deltaTime * MovementSpeed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, -180* Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, 180 * Time.deltaTime, 0);
+        }
+
+    }
+}
