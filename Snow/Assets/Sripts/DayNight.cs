2c96586272e374a536ef6aeb08b4b96adb3a6503
flyfisher22 <lance.s.chaplin@gmail.com>
2017-12-03 18:33:04 -0800

    sound and snow effects

---
+++ Snow/Assets/Sripts/DayNight.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNight : MonoBehaviour {
+
+    float MovementSpeed =2;
+
+	void Update () {
+        if (transform.rotation.eulerAngles.x >= 200 || transform.rotation.eulerAngles.x <= -15)
+        {
+            transform.Rotate(5 * Time.deltaTime, 0, 0);
+        }
+        else
+        {
+            transform.Rotate(MovementSpeed * Time.deltaTime, 0, 0);
+        }
+    }
+}
---
+++ Snow/Assets/Sripts/MusicControl.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MusicControl : MonoBehaviour {
+
+    public AudioClip SongOne;
+    public AudioClip SongTwo;
+    public AudioClip SongThree;
+
+    private AudioSource audioSource;
+    private float time = 0;
+    private float timePlayed = 0;
+    private int lastPlayed = 1;
+
+    void Start () {
+        audioSource = gameObject.GetComponent<AudioSource>();
+        PlaySongOne();
+    }
+	
+	public void PlaySongOne()
+    {
+        audioSource.Stop();
+        audioSource.loop = false;
+        audioSource.PlayOneShot(SongOne);
+        timePlayed += 169.968f;
+    }
+
+    public void PlaySongTwo()
+    {
+        audioSource.Stop();
+        audioSource.loop = false;
+        audioSource.PlayOneShot(SongTwo);
+        timePlayed += 265.848f;
+    }
+
+    public void PlaySongThree()
+    {
+        audioSource.Stop();
+        audioSource.loop = false;
+        audioSource.PlayOneShot(SongThree);
+        timePlayed += 91.992f;
+    }
+
+
+    void Update () {
+        time += Time.deltaTime;
+        
+        if (lastPlayed == 1 && time >= timePlayed)
+        {
+            PlaySongTwo();
+            lastPlayed = 2;
+        }
+        if (lastPlayed == 2 && time >= timePlayed)
+        {
+            PlaySongThree();
+            lastPlayed = 3;
+        }
+        if (lastPlayed == 3 && time >= timePlayed)
+        {
+            PlaySongOne();
+            lastPlayed = 1;
+        }
+    }
+}
